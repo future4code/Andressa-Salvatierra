8a679036aafa7118eb1a23cda11d8608c0356040
Andressa Flores <andressa.salvatierra@usp.br>
2019-11-21 19:53:26 -0300

    etapa 3 concluida

--- semana4/aula3-classes/blog/blog.js
+++ semana4/aula3-classes/blog/blog.js
@@ -5,15 +5,21 @@ class novoPost {
         this.conteudo = conteudo
     }
 }
+arrayPostsBlog = []
 
 function enviarPost() {
     let title = document.getElementById("titulo").value
     let author = document.getElementById("autor").value
     let content = document.getElementById("conteudo").value
     const conteudoPost = new novoPost(title, author, content)
-    console.log(title)
-    console.log(author)
-    console.log(content)
+    arrayPostsBlog.push(conteudoPost)
+    console.log(arrayPostsBlog)
+    document.getElementById("titulo").value = ""
+    document.getElementById("autor").value = ""
+    document.getElementById("conteudo").value = ""
+        // publicarPost()
 }
 
-console.log(novoPost)
+// function publicarPost() {
+
+// }
