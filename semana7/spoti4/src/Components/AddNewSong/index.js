e71ebb5fdf170b22e942d0c77109fafaf3b2c6c8
Andressa Flores <andressa.salvatierra@usp.br>
2019-12-13 16:51:42 -0300

    esqueletos estáticos e componentes

---
+++ semana7/spoti4/src/Components/AddNewSong/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+class AddNewSong extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+    
+}
+
+export default AddNewSong;
---
+++ semana7/spoti4/src/Components/Playlists/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+class Playlists extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+    
+}
+
+export default Playlists;
