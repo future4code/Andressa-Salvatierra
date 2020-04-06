39a05c93267c20a3523bec8d6192d4bfaaf19feb
Andressa Flores <andressa.salvatierra@usp.br>
2020-04-06 17:32:49 -0300

    renderizando Hello World

--- FutureTube/Frontend/futuretube-front/src/App.js
+++ FutureTube/Frontend/futuretube-front/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p></p>
+        <p>Hello world</p>
       </header>
     </div>
   );
