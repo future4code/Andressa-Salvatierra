c8e7a7ac05bca2d7bb3e64475412cd827c8c05c6
Andressa Flores <andressa.salvatierra@usp.br>
2020-04-07 20:20:03 -0300

    front configurado para hosting no Firebase

--- FutureTube/Frontend/futuretube-front/src/index.js
+++ FutureTube/Frontend/futuretube-front/src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9KAMtXQ5jUdzjFaivGBQ4nSdI1s-sb3c",
+  authDomain: "futuretube-246ef.firebaseapp.com",
+  databaseURL: "https://futuretube-246ef.firebaseio.com",
+  projectId: "futuretube-246ef",
+  storageBucket: "futuretube-246ef.appspot.com",
+  messagingSenderId: "483982462346",
+  appId: "1:483982462346:web:7340ef7027e1680e968c73",
+  measurementId: "G-YD98N20D26"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
