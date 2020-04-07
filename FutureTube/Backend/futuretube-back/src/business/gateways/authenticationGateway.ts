071ca8c2cc6e3fb321337ff2d1a9c4a405194251
Andressa Flores <andressa.salvatierra@usp.br>
2020-04-07 15:53:04 -0300

    estrutura de arquivos básica backend

---
+++ FutureTube/Backend/futuretube-back/src/business/gateways/authenticationGateway.ts
@@ -0,0 +1,12 @@
+
+
+export interface AuthenticationGateway {
+  generateToken(input: UsersInfoForToken): string;
+  getUsersInfoFromToken(token: string): UsersInfoForToken;
+}
+
+// userId, type
+
+export interface UsersInfoForToken {
+  userId: string;
+}
---
+++ FutureTube/Backend/futuretube-back/src/business/gateways/userGateway.ts
@@ -0,0 +1,9 @@
+import { User } from "../entities/user";
+
+export interface UserGateway {
+  createUser(user: User): Promise<void>;
+  getAllStudents(): Promise<User[]>;
+  getUserByEmail(email: string): Promise<User | undefined>;
+  getUserById(id: string): Promise<User | undefined>;
+  updatePassword(userId: string, newPassword: string): Promise<void>;
+}
