16f7016fc430d1c8dc3da1c1d4f1bdbb665daf69
Andressa Flores <andressa.salvatierra@usp.br>
2020-04-17 12:47:01 -0300

    header estático

--- FutureTube/Frontend/futuretube-front/src/containers/SignUp/index.js
+++ FutureTube/Frontend/futuretube-front/src/containers/SignUp/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { connect } from 'react-redux';
-import Header from "../Header"
+import Header from "../../components/Header"
 
 const SignUp = () => (
     <div>
--- FutureTube/Frontend/futuretube-front/src/style/theme.js
+++ FutureTube/Frontend/futuretube-front/src/style/theme.js
@@ -2,24 +2,13 @@ import { createMuiTheme } from "@material-ui/core";
 
 export default createMuiTheme({
   palette: {
-    type:'light',
+    type: 'light',
     primary: {
-       // light: will be calculated from palette.primary.main,
-      main: '#44318D',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
+      main: '#44318D'
     },
     secondary: {
-      main: '#aedaa3'
+      main: '#D83F87'
     } 
-    
-    // secondary: {
-    //   light: '#0066ff',
-    //   main: '#0044ff',
-    //   dark: will be calculated from palette.secondary.main,
-    //   contrastText: '#ffcc00',
-    // },
-    // error: will use the default color
     },
 });
    
